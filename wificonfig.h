bfc1152b3e0e76670a8d5abd0a2ac3d743c2b970
Edward Simpson <76537760+esimpson07@users.noreply.github.com>
2022-07-10 11:31:45 -0400

    Update wificonfig.h
    accidentally put wrong one

--- wificonfig.h
+++ wificonfig.h
@@ -1,40 +1,20 @@
-#include "wificonfig.h"
+#include <EEPROM.h>
+#include "Arduino.h"
 
-wificonfig::wificonfig() {
-  SSIDArray = (byte*)malloc(32);
-  PASSWORDArray = (byte*)malloc(32);
-}
+#define EEPROM_SIZE 64
 
-void wificonfig::load() {
-  EEPROM.begin(EEPROM_SIZE);
-  for(int i = 0; i < 32; i ++){
-    byte readEEPROM;
-    EEPROM.get(i,readEEPROM);
-    SSIDArray[i] = readEEPROM;
-  }
-  ssid = String((char*)SSIDArray);
-  for(int i = 32; i < 64; i ++){
-    byte readEEPROM;
-    EEPROM.get(i,readEEPROM);
-    PASSWORDArray[i - 32] = readEEPROM;
-  }
+class wificonfig {
+  public:
+    wificonfig();
+    
+    String ssid;
+    String password;
 
-  password = String((char*)PASSWORDArray);
-  EEPROM.end();
-}
+    void load();
+    void save();
+  private:
+    byte* SSIDArray;
+    byte* PASSWORDArray;
 
-void wificonfig::save() {
-  EEPROM.begin(EEPROM_SIZE);
-  for(int i = 0; i < ssid.length(); i ++){
-    EEPROM.put(i,stringToBytes(ssid,i));
-  }
-  for(int i = 32; i < (password.length() + 32); i ++){
-    EEPROM.put((i),stringToBytes(password,(i - 32)));
-  }
-  EEPROM.commit();
-  EEPROM.end();
-}
-
-int wificonfig::stringToBytes(String str, int character) {
-  return((byte)str.charAt(character));
-}
+    int stringToBytes(String str, int character);
+};
