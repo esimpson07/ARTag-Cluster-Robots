a5d07d35b69a9e66bbbeea989b93dcebdfc33472
Edward Simpson <76537760+esimpson07@users.noreply.github.com>
2022-07-11 17:38:10 -0400

    Update littlerobotdrive_old.ino
    Added a PID and rpm control to make robot drive in meters per second using ros

--- littlerobotdrive_old.ino
+++ littlerobotdrive_old.ino
@@ -7,17 +7,57 @@
 #define STACK_PROTECTOR  512
 #define MAX_SRV_CLIENTS 5
 
-#define ENA D2
-#define IN1 D3
-#define IN2 D4
-#define IN3 D7
-#define IN4 D8
-#define ENB D6
+#define ENA D4
+#define IN1 D2
+#define IN2 D3
+#define IN3 D6
+#define IN4 D7
+#define ENB D8
 
+#define EN1 D1
+#define EN2 D5
+ 
 int leftSpeed = 0;
 int rightSpeed = 0;
+int leftCount = 0;
+int rightCount = 0;
+int pastLeftCount = 0;
+int pastRightCount = 0;
+int leftRPM = 0;
+int rightRPM = 0;
 
-String sbuf;
+long currentTime = 0;
+long pastTime = 0;
+long timeDelay = 75;
+
+double retValL = 0;
+double retValR = 0;
+
+double previousTimeL = 0;
+double timeIntervalL = 0;
+double integralL = 0;
+double derivativeL = 0;
+double setPointL = 0;
+double errorL = 0;
+double previousErrorL = 0;
+double PL = 0;
+double IL = 0;
+double DL = 0;
+double valL = 0;
+
+double previousTimeR = 0;
+double timeIntervalR = 0;
+double integralR = 0;
+double derivativeR = 0;
+double setPointR = 0;
+double errorR = 0;
+double previousErrorR = 0;
+double PR = 0;
+double IR = 0;
+double DR = 0;
+double valR = 0;
+
+String sbuf = "510510";
 WiFiServer server(23); // the port the telnet connection is on
 WiFiClient serverClients[MAX_SRV_CLIENTS];
 
@@ -32,6 +72,13 @@ String id = "ESPsoftAP_10";
 String pw = "Soft_AP_PW";
 #endif
 
+/*
+ * Important things to know:
+ * - Uses telnet connection on ESP8266 12F
+ * - Runs on 2 TT motors with l298n
+ * - Max RPM on decent batteries ~ 210 RPM
+ * - 180 RPM = 1/3 m/s
+ */
 void setup() {
   pinMode(ENA,OUTPUT);
   pinMode(IN1,OUTPUT);
@@ -39,9 +86,26 @@ void setup() {
   pinMode(IN3,OUTPUT);
   pinMode(IN4,OUTPUT);
   pinMode(ENB,OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(EN1),encoderPulseL,CHANGE);
+  attachInterrupt(digitalPinToInterrupt(EN2),encoderPulseR,CHANGE);
   
   Serial.begin(115200);
+  initServer();
   
+  PIDControllerL(0.5,0.0003,0.0);
+  PIDControllerR(0.5,0.0003,0.0);
+  setSetPointL(0);
+  setSetPointR(0);
+}
+
+void loop() {
+  serverUpdate();
+  finalRPM();
+  leftDrive(calculateL(leftRPM));
+  rightDrive(calculateR(rightRPM));
+}
+
+void initServer() {
   #if SERVERMODE
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -69,19 +133,94 @@ void setup() {
   server.setNoDelay(true);
 }
 
-void loop() {
-  leftDrive(leftSpeed);
-  rightDrive(rightSpeed);
-  serverUpdate();
+ICACHE_RAM_ATTR void encoderPulseL() {
+  leftCount += leftSpeed / abs(leftSpeed);
+}
+
+ICACHE_RAM_ATTR void encoderPulseR() {
+  rightCount += rightSpeed / abs(rightSpeed);
+}
+
+double findRPM(double past,double current, double timeDifferential,double dir) {
+  return(dir * (current - past) * (60000 / (timeDifferential * 40)));
+}
+
+void finalRPM() {
+  currentTime = millis();
+  if(currentTime >= pastTime + timeDelay) {
+    //Serial.print("Left RPM = ");
+    leftRPM = findRPM(pastLeftCount,leftCount,timeDelay,(leftSpeed / abs(leftSpeed)));
+    //Serial.println(leftRPM);
+    //Serial.print("Right RPM = ");
+    rightRPM = findRPM(pastRightCount,rightCount,timeDelay,(rightSpeed / abs(rightSpeed)));
+    //Serial.println(rightRPM);
+    pastRightCount = rightCount;
+    pastLeftCount = leftCount;
+    pastTime = currentTime;
+  }
+}
+
+void PIDControllerL(double P, double I, double D) {
+  setPIDL(P,I,D);
+}
+
+void PIDControllerR(double P, double I, double D) {
+  setPIDR(P,I,D);
+}
+
+void setPIDL(double a, double b, double c) {
+  PL = a;
+  IL = b;
+  DL = c;
+}
+
+void setPIDR(double a, double b, double c) {
+  PR = a;
+  IR = b;
+  DR = c;
+}
+
+void setSetPointL(double value){
+  setPointL = value;
+}
+
+void setSetPointR(double value){
+  setPointR = value;
+}
+
+double calculateL(double actualL) {
+  timeIntervalL = currentTime - previousTimeL;
+  if(timeIntervalL > 10) {
+    errorL = (setPointL - actualL);
+    integralL += (errorL * timeIntervalL);
+    derivativeL = (errorL - previousErrorL) / timeIntervalL;
+    retValL = (PL * errorL) + (IL * integralL) + (DL * derivativeL);
+    previousErrorL = errorL;
+    previousTimeL = currentTime;
+  }
+  return(retValL);
+}
+
+double calculateR(double actualR) {
+  timeIntervalR = currentTime - previousTimeR;
+  if(timeIntervalR > 10) {
+    errorR = (setPointR - actualR);
+    integralR += (errorR * timeIntervalR);
+    derivativeR = (errorR - previousErrorR) / timeIntervalR;
+    retValR = (PR * errorR) + (IR * integralR) + (DR * derivativeR);
+    previousErrorR = errorR;
+    previousTimeR = currentTime;
+  }
+  return(retValR);
 }
 
 void leftDrive(int driveSpeed) {
   if(driveSpeed > 0) {
-    analogWrite(ENA,abs(driveSpeed));
+    analogWrite(ENA,abs(driveSpeed * 2.55));
     digitalWrite(IN1,LOW);
     digitalWrite(IN2,HIGH);
   } else if(driveSpeed < 0) {
-    analogWrite(ENA,abs(driveSpeed));
+    analogWrite(ENA,abs(driveSpeed * 2.55));
     digitalWrite(IN1,HIGH);
     digitalWrite(IN2,LOW);
   } else {
@@ -93,11 +232,11 @@ void leftDrive(int driveSpeed) {
 
 void rightDrive(int driveSpeed) {
   if(driveSpeed > 0) {
-    analogWrite(ENB,abs(driveSpeed));
+    analogWrite(ENB,abs(driveSpeed * 2.55));
     digitalWrite(IN3,LOW);
     digitalWrite(IN4,HIGH);
   } else if(driveSpeed < 0) {
-    analogWrite(ENB,abs(driveSpeed));
+    analogWrite(ENB,abs(driveSpeed * 2.55));
     digitalWrite(IN3,HIGH);
     digitalWrite(IN4,LOW);
   } else {
@@ -133,10 +272,10 @@ void serverUpdate(){
       size_t tcp_got = serverClients[i].read(buf, maxToSerial);
 
       sbuf = String((char*)buf);
-      leftSpeed = sbuf.substring(0,3).toInt() - 255;
-      rightSpeed = sbuf.substring(3,6).toInt() - 255;
-      Serial.println(leftSpeed);
-      Serial.println(rightSpeed);
+      setSetPointL((sbuf.substring(0,3).toInt() - 510) / 1.417);
+      setSetPointR((sbuf.substring(3,6).toInt() - 510) / 1.417);
+      Serial.println(setPointL);
+      Serial.println(setPointR);
     }
   }
 }
