19a8fad9d6d979968049f91a5efb32391e319a6b
Edward Simpson <76537760+esimpson07@users.noreply.github.com>
2022-07-23 20:51:06 -0400

    Create wifi_control.ino

---
+++ wifi_control.ino
@@ -0,0 +1,83 @@
+#include <ESP8266WiFi.h>
+#define MAX_SRV_CLIENTS 5
+
+String sbuf;
+
+WiFiServer server(23);
+WiFiClient serverClients[MAX_SRV_CLIENTS];
+
+const char* ssid = "PandoraAccessPoint";
+const char* password = "         ";
+
+int leftSpeed = 0;
+int rightSpeed = 0;
+
+int CMD1 = 0;
+int CMD2 = 0;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setTimeout(5);
+  serverInit();
+}
+
+void serverInit() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected, address=");
+  Serial.println(WiFi.localIP());
+  server.begin();
+  server.setNoDelay(true);
+}
+
+void serverUpdate(){
+  if (server.hasClient()) {
+    for (int i = 0; i < MAX_SRV_CLIENTS; i++) {
+      if (!serverClients[i]) {
+        serverClients[i] = server.available();
+        Serial.print("New client idx: ");
+        Serial.println(i);
+        break;
+      }
+
+      if (i >= MAX_SRV_CLIENTS) {
+        server.available().println("no sessions available");
+        Serial.print("Server exceeded max connections: ");
+        Serial.println(MAX_SRV_CLIENTS);
+      }
+    }
+  }
+
+  for (int i = 0; i < MAX_SRV_CLIENTS; i++) {
+    while (serverClients[i].available()) {
+      size_t maxToSerial = std::min(serverClients[i].available(), Serial.availableForWrite());
+      maxToSerial = std::min(maxToSerial, (size_t)512);
+      uint8_t buf[maxToSerial];
+      size_t tcp_got = serverClients[i].read(buf, maxToSerial);
+      sbuf = String((char*)buf);
+      Serial.println(sbuf);
+    }
+    if(Serial.available() > 0) {
+      serverClients[i].println(Serial.readString());
+    }
+  }
+}
+
+void parseString(String inputMessage) {
+  int CMD1POS = inputMessage.indexOf("1:");
+  int CMD2POS = inputMessage.indexOf("2:");
+  int ENDPOS = inputMessage.indexOf("E;");
+  
+  CMD1 = inputMessage.substring(CMD1POS + 2,CMD2POS).toInt();
+  CMD2 = inputMessage.substring(CMD2POS + 2,ENDPOS).toInt();
+}
+
+void loop() {
+  serverUpdate();
+}
